8e9acf0fde3cd91d87c1b06c8221c22d68126cc6
Jan H. Krüger <janhkrueger@outlook.com>
2023-08-24 13:46:53 +0200

    Load yaml file based on parameter

--- public/main.js
+++ public/main.js
@@ -16,74 +16,76 @@ function getPriorityColor(priority) {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-// Load YAML file via fetch API
-fetch('bookmarks.yaml')
-  .then(response => response.text())
-  .then(yamlText => {
-    // Parse YAML text to JavaScript object
-    const doc = jsyaml.load(yamlText);
-
-    // Sort categories by priority and then by name
-    doc.categories.sort((a, b) => {
-      if (a.priority === b.priority) {
-        return a.name.localeCompare(b.name);
-      }
-      return a.priority - b.priority;
-    });
-
-    const output = document.getElementById('output');
-
-    // Iterate through each (sorted) category
-    doc.categories.forEach((category) => {
-      const categoryContainer = document.createElement("div");
-
-      // Create a container for the category title and image
-      const titleContainer = document.createElement("div");
-      titleContainer.style.display = 'flex';
-      titleContainer.style.alignItems = 'center';
-
-      // Create an image element only if the image field exists
-      if (category.image) {
-        const categoryImage = document.createElement("img");
-        categoryImage.src = category.image;
-        categoryImage.alt = category.name + " icon";
-        categoryImage.style.width = '20px';  // You can customize this
-        categoryImage.style.marginRight = '10px';  // Add some space between the image and the title
-        titleContainer.appendChild(categoryImage);
-      }
-
-      // Create category title
-      const categoryHeading = document.createElement("h2");
-      categoryHeading.textContent = category.name;
-      titleContainer.appendChild(categoryHeading);
-
-      // Append titleContainer to categoryContainer
-      categoryContainer.appendChild(titleContainer);
-
-      // Create category box
-      const categoryBox = document.createElement("div");
-      categoryBox.className = "category-box";
-
-      // Set the background color based on priority
-      categoryBox.style.backgroundColor = getPriorityColor(category.priority);
-
-      categoryContainer.appendChild(categoryBox);
-
-      const linkList = document.createElement("ul");
-      category.links.forEach((link) => {
-        const listItem = document.createElement("li");
-        const anchor = document.createElement("a");
-        anchor.href = link.url;
-        anchor.textContent = link.title;
-        anchor.style.color = 'black'; // Ensure the link color is black
-        listItem.appendChild(anchor);
-        linkList.appendChild(listItem);
+// Function to fetch and load YAML
+function loadYAML(yamlSource) {
+  fetch(yamlSource)
+    .then(response => response.text())
+    .then(yamlText => {
+      const doc = jsyaml.load(yamlText);
+      doc.categories.sort((a, b) => {
+        if (a.priority === b.priority) {
+          return a.name.localeCompare(b.name);
+        }
+        return a.priority - b.priority;
       });
 
-      categoryBox.appendChild(linkList);
-      output.appendChild(categoryContainer);
+      const output = document.getElementById('output');
+      doc.categories.forEach((category) => {
+        const categoryContainer = document.createElement("div");
+        const titleContainer = document.createElement("div");
+        titleContainer.style.display = 'flex';
+        titleContainer.style.alignItems = 'center';
+
+        if (category.image) {
+          const categoryImage = document.createElement("img");
+          categoryImage.src = category.image;
+          categoryImage.alt = category.name + " icon";
+          categoryImage.style.width = '20px';
+          categoryImage.style.marginRight = '10px';
+          titleContainer.appendChild(categoryImage);
+        }
+
+        const categoryHeading = document.createElement("h2");
+        categoryHeading.textContent = category.name;
+        titleContainer.appendChild(categoryHeading);
+
+        categoryContainer.appendChild(titleContainer);
+
+        const categoryBox = document.createElement("div");
+        categoryBox.className = "category-box";
+        categoryBox.style.backgroundColor = getPriorityColor(category.priority);
+        categoryBox.style.color = 'black';
+
+        categoryContainer.appendChild(categoryBox);
+
+        const linkList = document.createElement("ul");
+        category.links.forEach((link) => {
+          const listItem = document.createElement("li");
+          const anchor = document.createElement("a");
+          anchor.href = link.url;
+          anchor.textContent = link.title;
+          anchor.style.color = 'black';
+          listItem.appendChild(anchor);
+          linkList.appendChild(listItem);
+        });
+
+        categoryBox.appendChild(linkList);
+        output.appendChild(categoryContainer);
+      });
+    })
+    .catch((error) => {
+      console.error('There was an error:', error);
     });
-  })
-  .catch((error) => {
-    console.error('There was an error:', error);
-  });
+}
+
+// Load YAML when the document is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  const outputElement = document.getElementById('output');
+  const yamlSource = outputElement.getAttribute('data-yaml-source');
+
+  if (yamlSource) {
+    loadYAML(yamlSource);
+  } else {
+    console.error('No YAML source specified.');
+  }
+});
