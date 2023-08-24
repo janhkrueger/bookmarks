cb7482b8f6a3e386aef9e1ca1fb843f2c2a132cf
Jan H. Krüger <janhkrueger@outlook.com>
2023-08-24 09:29:41 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// Load YAML file via fetch API
+fetch('bookmarks.yaml')
+  .then(response => response.text())
+  .then(yamlText => {
+    // Parse YAML text to JavaScript object
+    const doc = jsyaml.load(yamlText);
+    const output = document.getElementById('output');
+
+    // Iterate through each category
+    doc.categories.forEach((category) => {
+      const categoryContainer = document.createElement("div");
+
+      // Create a container for the category title and image
+      const titleContainer = document.createElement("div");
+      titleContainer.style.display = 'flex';
+      titleContainer.style.alignItems = 'center';
+
+      // Create an image element
+      const categoryImage = document.createElement("img");
+      categoryImage.src = category.image;
+      categoryImage.alt = category.name + " icon";
+      categoryImage.style.width = '50px'; // You can customize this
+      categoryImage.style.marginRight = '10px'; // Add some space between the image and the title
+      titleContainer.appendChild(categoryImage);
+
+      // Create category title
+      const categoryHeading = document.createElement("h2");
+      categoryHeading.textContent = category.name;
+      titleContainer.appendChild(categoryHeading);
+
+      // Append titleContainer to categoryContainer
+      categoryContainer.appendChild(titleContainer);
+
+      // Create category box
+      const categoryBox = document.createElement("div");
+      categoryBox.className = "category-box";
+      categoryContainer.appendChild(categoryBox);
+
+      const linkList = document.createElement("ul");
+      category.links.forEach((link) => {
+        const listItem = document.createElement("li");
+        const anchor = document.createElement("a");
+        anchor.href = link.url;
+        anchor.textContent = link.title;
+        listItem.appendChild(anchor);
+        linkList.appendChild(listItem);
+      });
+
+      categoryBox.appendChild(linkList);
+      output.appendChild(categoryContainer);
+    });
+  })
+  .catch((error) => {
+    console.error('There was an error:', error);
+  });
